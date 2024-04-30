fe8d893c952237a33d56554928b24082356ce7e7
Charith <herath.charithjay@gmail.com>
2024-04-30 11:12:28 +0530

    added -y

--- jenkins-setup.sh
+++ jenkins-setup.sh
@@ -2,7 +2,7 @@
 
 # installing java
 sudo apt update
-sudo apt install fontconfig openjdk-17-jre
+sudo apt install fontconfig openjdk-17-jre -y
 java -version
 
 #install jenkins if java is available
@@ -10,5 +10,5 @@ if [ $? -eq 0 ]; then
     sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
     echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
     sudo apt-get update
-    sudo apt-get install jenkins
+    sudo apt-get install jenkins -y
 fi
